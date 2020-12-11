f1d27330ad714c02f8cd10f817f2c0c5e0882e62
Amir Sahil <48803384+AmirSahil@users.noreply.github.com>
2020-12-12 00:47:49 +0530

    refresh

--- flaskblog.py
+++ flaskblog.py
@@ -1,22 +1,19 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '8616402e032397f4bf56196dbcf8c4cf'
-
 posts = [
     {
-        'author': 'Amir Sahil',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'October 7, 2020'
+        'author' : 'Amir Sahil',
+        'title' : 'Blog Post 1',
+        'content' : 'First post content',
+        'date_posted' : '12th December 2020'
     },
     {
-        'author': 'Dheeraj Ponnaganti',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'October 9, 2020'
+        'author' : 'Dheeraj Ponnaganti',
+        'title' : 'Blog Post 2',
+        'content' : 'Secont post content',
+        'date_posted' : '12th December 2020'
     }
 ]
 
@@ -24,28 +21,11 @@ posts = [
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', posts=posts)
-
+    return render_template('home.html', blogposts=posts)
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
 
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
-    return render_template('register_my_code.html', title='Register', form=form)
-
-
-@app.route('/login')
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='Login', form=form)
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
