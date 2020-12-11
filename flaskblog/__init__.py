ff6c34a2dc4691bb677cd268d71ceba9f5066f9f
Amir Sahil <48803384+AmirSahil@users.noreply.github.com>
2020-12-12 04:43:35 +0530

    registration db page

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 
@@ -7,5 +8,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a6988613e1fa25983b7264f202c9e499'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
